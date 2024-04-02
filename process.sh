73c7cdcf4d1baae6f4b25c3275b5be266ef68e89
ibrar Ullah <ibrar.ullah@aioapp.com>
2024-04-03 01:43:42 +0500

    new video for FLAGRANT1

--- process.sh
+++ process.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Directory containing the original videos
-input_dir="AdinRoss"
+input_dir="FLAGRANT1"
 
 # Directory where the processed videos will be saved
-output_dir="AdinRoss_final"
+output_dir="FLAGRANT_final1"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
@@ -18,7 +18,7 @@ for input_file in "$input_dir"/*.mp4; do
   output_file="$output_dir/${base_name}.mp4"
   
   # Apply the FFmpeg filter
-  ffmpeg -i "$input_file" -vf "eq=contrast=1.2:brightness=0.1:saturation=1.1, colorbalance=rs=-0.05:gs=0:bs=0.1, crop=iw*0.85:ih*0.85:iw*0.125:ih*0.125, scale=1920:1080" "$output_file"
+  ffmpeg -i "$input_file" -vf "curves=all='0/0 0.25/0.15 0.5/0.5 0.75/0.85 1/1', eq=saturation=1.2, unsharp=7:7:2.5:7:7:0.5, crop=iw*0.90:ih*0.90:iw*0.125:ih*0.125, scale=1920:1080" -c:a copy "$output_file"
   
   echo "Processed $input_file and saved to $output_file"
 done
