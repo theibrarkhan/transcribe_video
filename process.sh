9cb51af0b7c18f748667630bf0fb24d4baf7761b
ibrar Ullah <ibrar.ullah@aioapp.com>
2024-04-22 23:56:07 +0500

    VirallyRay1

--- process.sh
+++ process.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Directory containing the original videos
-input_dir="Emiru"
+input_dir="VirallyRay"
 
 # Directory where the processed videos will be saved
-output_dir="Emiru2"
+output_dir="VirallyRay1"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
